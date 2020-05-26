12705c8b3312558b7edb9e92debe0af66c1598b6
Taimin <taiminyang@gmail.com>
2020-05-26 20:42:00 +0200

    Major change, removed all the test and example files.

--- modules/cctbx_project/cctbx/command_line/cod_select_and_pickle.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, division, print_function
-if (__name__ == "__main__"):
-  from cctbx.omz.cod_select_and_pickle import run
-  import libtbx.load_env
-  import sys
-  run(args=sys.argv[1:], command_name=libtbx.env.dispatcher_name)
